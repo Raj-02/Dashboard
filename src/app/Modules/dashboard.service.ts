cb44aa146bb49b2cb3b6cc54d9fe53ec2af204c6
Singh <Rajkumar.Singh@ext.saint-gobain.com>
2020-06-29 15:21:05 +0530

    Table Layout

--- src/app/Modules/dashboard.service.ts
+++ src/app/Modules/dashboard.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
   constructor() { }
+
+  Bigchart() {
+    return[1, 2, 3, 4, 5, 3, 6];
+   }
+  Piechart(){
+     return[{name: 'C drive',
+     y: 500,
+     selected: true
+ }, {
+     name: 'D drive',
+     y: 300
+ }, {
+     name: 'E drive',
+     y: 350
+ }, {
+     name: 'F drive',
+     y: 400}];
+  }
 }
